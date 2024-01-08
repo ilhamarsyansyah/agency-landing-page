dcb1a59f9263644de64a30c00a8baa27d3fcceca
ilhamarsyansyah <arsyansyah86@gmail.com>
2024-01-08 17:50:07 +0700

    Upload Project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: '#331B3B',
+        secondaryColor: '#FFF',
+      }
+    },
+  },
+  plugins: [],
+}
